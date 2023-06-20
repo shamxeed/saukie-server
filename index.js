01bce3485eb68c7ae83ddf732e396978222ac259
shamxeed <shamxeedmuhd@gmail.com>
2023-06-20 20:01:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
