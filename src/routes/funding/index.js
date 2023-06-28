af4f8cb8cd34076d85d2537e25d387b42f898a44
shamxeed <shamxeedmuhd@gmail.com>
2023-06-28 19:41:17 +0100

    .

--- src/routes/funding/index.js
+++ src/routes/funding/index.js
@@ -4,7 +4,9 @@ const controllers = require('../../controllers/funding');
 
 const router = express.Router();
 
-router.post('/automated/vpay', controllers.vpay);
+router.post('/automated/vpay', (r, res) =>
+  res.send('This route is been temporarily blocked')
+);
 
 router.post('/automated/monnify', controllers.monnify);
 
